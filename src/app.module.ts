ad36b53fe30beb61cac55ee74c95de47a955e34b
jinhyo <sosilion@naver.com>
2022-04-09 15:48:44 +0900

    GraphQL 초기 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MorganInterceptor, MorganModule } from 'nest-morgan';
+import { join } from 'path';
 import { DatabaseModule } from './modules/database/database.module';
 import { MyConfigModule } from './modules/my-config/my-config.module';
 
 @Module({
-  imports: [MorganModule, MyConfigModule, DatabaseModule],
+  imports: [
+    MorganModule,
+    MyConfigModule,
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
