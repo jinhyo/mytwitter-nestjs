68ac73097b74e44c3465e0e51853734a1ff68ea6
jinhyo <sosilion@naver.com>
2022-05-12 15:06:03 +0900

    로그아웃
    - User Resolver : Query - logout
    - DTO : SuccessDTO

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -1,5 +1,7 @@
-import { UseGuards } from '@nestjs/common';
+import { Res, UseGuards } from '@nestjs/common';
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Response } from 'express';
+import { SuccessDTO } from 'src/commonDTOs/success.dto';
 import { LoginUser } from 'src/decorators/loginUser.decorator';
 import { User } from 'src/entities/user.entity';
 import { LoginGuard } from 'src/guards/login.guard';
@@ -10,6 +12,13 @@ import { UserService } from './user.service';
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
+
+  @Query(() => SuccessDTO)
+  @UseGuards(LoginGuard)
+  logout(@Context() context): SuccessDTO {
+    context.res.cookie('token', '', { maxAge: 0 });
+    return { success: true };
+  }
 
   @Query(() => User)
   @UseGuards(LoginGuard)
